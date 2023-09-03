2aae426d28a7f21a13f81d324e4251c4efc610b5
Anna Sudol <sudolann@gmail.com>
2023-09-03 20:36:07 +0200

    Added Form

---
+++ src/lib/components/SendEthForm.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let maxValue = [];
+</script>
+
+
+<form on:submit|preventDefault>
+  <div class="form-section">
+    <label for="address">Eth address</label>
+    <input type="text" id="address" placeholder="ETH address" />
+  </div>
+
+  <div class="form-section">
+    <label for="amount">Amount</label>
+    <input type="number" id="amount" placeholder="0.5" max={maxValue} />
+  </div>
+
+</form>
